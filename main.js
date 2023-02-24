94797df24a0f29fd7a89564b36b929f21cc49735
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-24 15:09:21 +0100

    squareDom

---
+++ main.js
@@ -0,0 +1,24 @@
+/* 
+- un programma che stampi in console i numeri da 1 a 100,
+- ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+- e per i multipli di 5 stampi “Buzz”.
+- Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. 
+*/
+
+
+
+const containerDom = document.querySelector('.container');
+
+for ( let i = 1; i <= 100; i++ ) {
+
+    const squareDom = `<div class="square">${i}</div>`;
+    console.log(i)
+
+    containerDom.innerHTML += squareDom;
+
+    if(i % 3 == 0) {
+        console.log('Frizzy')
+    }
+
+}
+
