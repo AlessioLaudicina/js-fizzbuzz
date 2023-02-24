1c8ec87ee93b88a36a82a475808cd556d7aa6505
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-24 16:24:36 +0100

    numeri divisibili per 15 console.log

--- main.js
+++ main.js
@@ -16,14 +16,23 @@ for ( let i = 1; i <= 100; i++ ) {
 
     containerDom.innerHTML += squareDom;
 
-    if( i % 3 == 0) {
-        console.log('Frizzy')
+    if( i % 15 == 0) {
+        console.log('FrizzBuzz')
         
+        
+    }
+
+     else if( i % 3 == 0){
+        console.log('Frizzy')
     }
 
     else if( i % 5 == 0){
         console.log('Buzz')
+
     }
+
+
+    
 
     else{
         console.log(i)
